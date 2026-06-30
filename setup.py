9cc3b4a2c606de328dd3353c4d9b793c636e1b0b
Nocyl <157864652+Nocyl@users.noreply.github.com>
2026-06-30 08:20:12 +0000

    feat: initial release moneypulse v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="moneypulse",
-    version='2.0.0',
+    version="2.0.0",
     description="Official Money-Pulse Python SDK — Accept payments and process payouts across Africa.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
